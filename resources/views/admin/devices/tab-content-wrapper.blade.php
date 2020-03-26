e51ad25c2bdf56c3782022e93be8f3188d4ecd34
unknown <sidorov.mihail.maykop@mail.ru>
2020-03-26 09:58:04 +0300

    Привел в порядок таблицу дополнительной информации устройства.

--- resources/views/admin/devices/tab-content-wrapper.blade.php
+++ resources/views/admin/devices/tab-content-wrapper.blade.php
@@ -16,6 +16,44 @@
     <div class="tab-content-wrapper__title">Список:</div>
     <div class="tab-content-wrapper__list">
         @foreach ($devices as $device)
+            <?php
+                $ts = $device->receipt_date;
+                $d = new DateTime();
+                $d->setTimestamp($ts);
+                $device->receipt_date = $d->format('d-m-Y');
+
+                $ts = $device->warranty;
+                $d = new DateTime();
+                $d->setTimestamp($ts);
+                $device->warranty = $d->format('d-m-Y');
+
+                switch ($device->type_device_id) {
+                    case 1:
+                        $type_device = 'Портативный';
+                        break;
+                    case 2:
+                        $type_device = 'Рабочее место';
+                        break;
+                    case 3:
+                        $type_device = 'Переферия';
+                        break;
+                    case 4:
+                        $type_device = 'Оргтехника';
+                        break;
+                }
+                
+                switch ($device->status) {
+                    case 'store':
+                        $status = 'На складе';
+                        break;
+                    case 'given':
+                        $status = 'Выдан';
+                        break;
+                    case 'write_off':
+                        $status = 'Списан';
+                        break;
+                }
+            ?>
             <div class="tab-content-wrapper__list-item" id="{{ $device->id }}">
                 <div class="tab-content-wrapper__list-item-head">
                     <div class="tab-content-wrapper__list-item-name">{{ $device->name }}</div>
@@ -31,7 +69,7 @@
                                 <td>Тип</td>
                                 <td>Дата поступления</td>
                                 <td>Закупочная цена</td>
-                                <td>Срок гарантии (мес.)</td>
+                                <td>Срок гарантии (дата окончания)</td>
                                 <td>Выдано сотруднику</td>
                                 <td>Ответственный</td>
                                 <td>Поставщик</td>
@@ -42,14 +80,14 @@
                         <tr>
                             <td>{{ $device->model }}</td>
                             <td>{{ $device->serial_number }}</td>
-                            <td>{{ $device->type_device_id }}</td>
+                            <td>{{ $type_device }}</td>
                             <td>{{ $device->receipt_date }}</td>
                             <td>{{ $device->purchase_price }}</td>
                             <td>{{ $device->warranty }}</td>
                             <td>{{ $device->worker_id }}</td>
                             <td>{{ $device->provider_id }}</td>
                             <td>{{ $device->responsible_id }}</td>
-                            <td>{{ $device->status }}</td>
+                            <td>{{ $status }}</td>
                         </tr>
                         </tbody>
                     </table>
