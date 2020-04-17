ff6b7aafaec94b27274dc41199e802e202e5bf05
Mihail <sidorov.mihail.maykop@mail.ru>
2020-04-17 18:53:14 +0300

    Обновил БД.

--- DB.sql
+++ DB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Мар 26 2020 г., 09:29
+-- Время создания: Апр 17 2020 г., 17:51
 -- Версия сервера: 10.4.11-MariaDB
 -- Версия PHP: 7.4.3
 
@@ -25,6 +25,48 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `description`) VALUES
+(3, 'Категория 1', 'Описание категории 1'),
+(4, 'Категория 2', 'Описание категории 2\r\nОписание категории 2\r\nОписание категории 2');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `departments`
+--
+
+CREATE TABLE `departments` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `departments`
+--
+
+INSERT INTO `departments` (`id`, `name`, `description`) VALUES
+(1, 'Отдел 1', 'Описание отдела 1'),
+(2, 'Отдел 2', 'Описание отдела 2'),
+(3, 'Отдел 3', 'Описание отдела 3'),
+(4, 'Отдел 4', 'Описание отдела 4');
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `devices`
 --
 
@@ -33,23 +75,53 @@ CREATE TABLE `devices` (
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `model` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `serial_number` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `inventar_number` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `type_device_id` int(11) NOT NULL,
   `receipt_date` int(11) NOT NULL,
   `purchase_price` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `warranty` int(11) NOT NULL,
-  `worker_id` int(11) DEFAULT NULL,
-  `provider_id` int(11) DEFAULT NULL,
-  `responsible_id` int(11) DEFAULT NULL,
-  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'store'
+  `responsible_id` int(11) NOT NULL,
+  `provider_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Дамп данных таблицы `devices`
 --
 
-INSERT INTO `devices` (`id`, `name`, `model`, `serial_number`, `type_device_id`, `receipt_date`, `purchase_price`, `warranty`, `worker_id`, `provider_id`, `responsible_id`, `status`) VALUES
-(3, 'Имя устройства 11', 'Модель устройства 11', 'Серийный номер устройства 11', 1, 1585170000, '10001', 1585256400, NULL, NULL, NULL, 'store'),
-(4, 'Имя устройства 22', 'Модель устройства 22', 'Серийный номер устройства 22', 3, 1585083600, '20002', 1585515600, NULL, NULL, NULL, 'store');
+INSERT INTO `devices` (`id`, `name`, `model`, `serial_number`, `inventar_number`, `type_device_id`, `receipt_date`, `purchase_price`, `warranty`, `responsible_id`, `provider_id`, `category_id`) VALUES
+(1, 'Устройство 1', 'Модель устройства 1', 'Серийный номер устройства 1', '', 1, 1586984400, '1000', 1587157200, 1, 1, 4),
+(2, 'Устройство 2', 'Модель устройства 2', 'Серийный номер устройства 2', '11111', 2, 1585515600, '2000', 1586984400, 1, 2, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `device_worker`
+--
+
+CREATE TABLE `device_worker` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `device_id` int(11) NOT NULL,
+  `worker_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `attach` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `device_worker`
+--
+
+INSERT INTO `device_worker` (`id`, `device_id`, `worker_id`, `created_at`, `updated_at`, `attach`) VALUES
+(1, 1, 1, '2020-04-17 12:39:09', '2020-04-17 12:39:24', 0),
+(2, 1, 2, '2020-04-17 12:51:10', '2020-04-17 14:14:39', 0),
+(3, 2, 4, '2020-04-17 12:51:59', '2020-04-17 14:15:46', 0),
+(4, 1, 1, '2020-04-17 14:16:16', '2020-04-17 14:16:30', 0),
+(5, 1, 3, '2020-04-17 14:16:40', '2020-04-17 14:20:15', 0),
+(6, 2, 4, '2020-04-17 14:17:18', '2020-04-17 14:20:23', 0),
+(7, 1, 1, '2020-04-17 14:25:03', '2020-04-17 14:26:42', 0),
+(8, 1, 4, '2020-04-17 14:48:22', '2020-04-17 15:38:23', 0),
+(9, 2, 3, '2020-04-17 14:49:56', '2020-04-17 15:38:25', 0);
 
 -- --------------------------------------------------------
 
@@ -87,8 +159,13 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
 (4, '2020_03_18_125119_add_role_field_to_users', 1),
-(6, '2020_03_20_120059_create_devices_table', 2),
-(7, '2020_03_26_081100_create_workers_table', 3);
+(6, '2020_03_26_081100_create_workers_table', 1),
+(8, '2020_03_30_072112_create_responsibles_table', 3),
+(10, '2020_03_30_115524_create_departments_table', 4),
+(11, '2020_03_27_115502_create_providers_table', 5),
+(15, '2020_04_13_072136_create_device_worker_table', 6),
+(20, '2020_04_16_082310_create_categories_table', 8),
+(21, '2020_03_20_120059_create_devices_table', 9);
 
 -- --------------------------------------------------------
 
@@ -101,6 +178,49 @@ CREATE TABLE `password_resets` (
   `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `providers`
+--
+
+CREATE TABLE `providers` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `providers`
+--
+
+INSERT INTO `providers` (`id`, `name`, `description`) VALUES
+(1, 'Поставщик 1', 'Описание поставщика 1'),
+(2, 'Поставщик 2', 'Описание поставщика 2'),
+(3, 'Поставщик 3', 'Описание поставщика 3');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `responsibles`
+--
+
+CREATE TABLE `responsibles` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `post` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `department_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `responsibles`
+--
+
+INSERT INTO `responsibles` (`id`, `name`, `post`, `department_id`) VALUES
+(1, 'Ответственный 1', 'Должность 1', 1),
+(2, 'Ответственный 2', 'Должность 2', 2),
+(3, 'Ответственный 3', 'Должность 3', 3);
 
 -- --------------------------------------------------------
 
@@ -125,7 +245,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `role`) VALUES
-(1, 'Name', 'mail@mail.ru', NULL, '$2y$10$wJSUKpBgBs4zi9BkkJS0quxU3S/dW2N663F4HWHBnfSqQvZ752sxG', 'TWLBjmQNSaTr8lS91bvK8qblmYpiDo9aFC02EwY3brlW40LOV5YO2x2pQ3CG', '2020-03-18 11:57:09', '2020-03-18 11:57:09', 'admin');
+(1, 'Имя', 'mail@mail.ru', NULL, '$2y$10$Xdew/aCqtbSKBGc1/jzSmecSoiA9WXfDpBTXLNFO.jwIyk9NJ1raW', NULL, '2020-03-27 08:28:53', '2020-03-27 08:28:53', 'admin');
 
 -- --------------------------------------------------------
 
@@ -146,16 +266,36 @@ CREATE TABLE `workers` (
 
 INSERT INTO `workers` (`id`, `name`, `post`, `department_id`) VALUES
 (1, 'Сотрудник 1', 'Должность 1', 1),
-(2, 'Сотрудник 2', 'Должность 2', 2);
+(2, 'Сотрудник 2', 'Должность 2', 2),
+(3, 'Сотрудник 3', 'Должность 3', 3),
+(4, 'Сотрудник 4', 'Должность 4', 4);
 
 --
 -- Индексы сохранённых таблиц
 --
 
 --
+-- Индексы таблицы `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `departments`
+--
+ALTER TABLE `departments`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Индексы таблицы `devices`
 --
 ALTER TABLE `devices`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `device_worker`
+--
+ALTER TABLE `device_worker`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -177,6 +317,18 @@ ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
 
 --
+-- Индексы таблицы `providers`
+--
+ALTER TABLE `providers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `responsibles`
+--
+ALTER TABLE `responsibles`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Индексы таблицы `users`
 --
 ALTER TABLE `users`
@@ -194,10 +346,28 @@ ALTER TABLE `workers`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблицы `departments`
+--
+ALTER TABLE `departments`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT для таблицы `devices`
 --
 ALTER TABLE `devices`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `device_worker`
+--
+ALTER TABLE `device_worker`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT для таблицы `failed_jobs`
@@ -209,7 +379,19 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT для таблицы `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT для таблицы `providers`
+--
+ALTER TABLE `providers`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `responsibles`
+--
+ALTER TABLE `responsibles`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
@@ -221,7 +403,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `workers`
 --
 ALTER TABLE `workers`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
